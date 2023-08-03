a1592dd88cb3cfd57b8684c9866956ab14ae305d
Benjamin <Bigonzalez@miuandes.cl>
2023-08-03 04:35:35 +0200

    all first Tables from assigment 1

--- nombre_del_proyecto/app.js
+++ nombre_del_proyecto/app.js
@@ -1,3 +1,5 @@
+// const y librerias ------------------
+
 const express = require('express');
 const cassandra = require('cassandra-driver');
 const path = require('path');
@@ -8,6 +10,9 @@ app.use(express.json());
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+// ------------------------------------
+
+// coneccion con cassandra -----------------
 // Configuración para servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -42,7 +47,8 @@ client.execute(createKeyspaceQuery, (err) => {
     console.log('Keyspace creado o ya existente');
   }
 });
-
+// ------------------------------
+// Crear Tablas ----------------------------------------
 // Crea la tabla "usuarios" si no existe en el keyspace
 const createTableQueryUsuarios = `
   CREATE TABLE IF NOT EXISTS mi_keyspace.usuarios (
@@ -98,7 +104,8 @@ CREATE TABLE IF NOT EXISTS mi_keyspace.salesbyyear (
   sales int
 )
 `;
-
+// ----------------------------------------------
+// coneccion con tablas --------------------------
 client.execute(createTableQueryUsuarios, (err) => {
   if (err) {
     console.error('Error al crear la tabla usuarios:', err);
@@ -109,25 +116,46 @@ client.execute(createTableQueryUsuarios, (err) => {
       if (err) {
         console.error('Error al crear la tabla usuarios2:', err);
       } else {
-
+        console.log('Tabla usuarios2 creada o ya existente');
       }
     })
-    console.log('Tabla usuarios2 creada o ya existente');
+   
     // Ahora ejecutas el query para la segunda tabla
+    client.execute(createTableQueryBooks, (err) => {
+      if (err) {
+        console.error('Error al crear la tabla books:', err);
+      } else {
+        console.log('Tabla books creada o ya existente');
+      }
+    });
+  // Ahora ejecutas el query para la segunda tabla
     client.execute(createTableQueryAuthors, (err) => {
       if (err) {
         console.error('Error al crear la tabla Autores:', err);
       } else {
         console.log('Tabla Autores creada o ya existente');
       }
-    });;
+    });
+    client.execute(createTableQueryReviews, (err) => {
+      if (err) {
+        console.error('Error al crear la tabla Reviews:', err);
+      } else {
+        console.log('Tabla Review creada o ya existente');
+      }
+    });
+    client.execute(createTableQuerySalesByYear, (err) => {
+      if (err) {
+        console.error('Error al crear la tabla Sales_By_Year:', err);
+      } else {
+        console.log('Tabla Sales_By_Year creada o ya existente');
+      }
+    });
   }
 });
 
-
-// Resto del código igual que antes
-
-// Ruta para crear un nuevo usuario
+// -------------------------------------------
+// Get y Post -------------------------------------
+// Ruta para crear un nuevo usuario (Ejemplo)
 app.post('/usuarios2', (req, res) => {
   const { id, nombre } = req.body;
 
@@ -146,7 +174,21 @@ app.post('/usuarios2', (req, res) => {
     }
   });
 });
-// Ruta para crear un nuevo usuario
+// Ruta para obtener todos los usuarios(Ejemplo)
+app.get('/usuarios2', (req, res) => {
+  const selectQuery = 'SELECT * FROM usuarios2';
+
+  client.execute(selectQuery, [], (err, result) => {
+    if (err) {
+      console.error('Error al obtener los usuarios:', err);
+      res.status(500).send('Error al obtener los usuarios');
+    } else {
+      // Renderiza la plantilla Pug y pasa los datos de usuarios a la vista
+      res.render('usuarios2', { users: result.rows });
+    }
+  });
+});
+// Ruta para crear un nuevo Autor
 app.post('/Authors', (req, res) => {
   const { id, nombre, dateOfBirth, country, shortDescription} = req.body;
 
@@ -165,22 +207,7 @@ app.post('/Authors', (req, res) => {
     }
   });
 });
-// Ruta para obtener todos los usuarios
-app.get('/usuarios2', (req, res) => {
-  const selectQuery = 'SELECT * FROM usuarios2';
-
-  client.execute(selectQuery, [], (err, result) => {
-    if (err) {
-      console.error('Error al obtener los usuarios:', err);
-      res.status(500).send('Error al obtener los usuarios');
-    } else {
-      // Renderiza la plantilla Pug y pasa los datos de usuarios a la vista
-      res.render('usuarios2', { users: result.rows });
-    }
-  });
-});
-
-// Ruta para obtener todos los usuarios
+// Ruta para obtener todos los Autores
 app.get('/Authors', (req, res) => {
   const selectQuery = 'SELECT * FROM authors';
 
@@ -194,8 +221,108 @@ app.get('/Authors', (req, res) => {
     }
   });
 });
+// Ruta para crear un nuevo Libro
+app.post('/Books', (req, res) => {
+  const { id, nombre, summary, dateOfPublication, numberOfSales} = req.body;
 
+  const insertQuery = `
+    INSERT INTO Books (id, nombre,summary, dateOfPublication, numberOfSales)
+    VALUES (?, ?, ?, ? ,?)
+  `;
 
+  client.execute(insertQuery, [id, nombre, summary, dateOfPublication, numberOfSales], { prepare: true }, (err) => {
+    if (err) {
+      console.error('Error al crear el Books:', err);
+      res.status(500).send('Error al crear el Books');
+    } else {
+      console.log('Books creado exitosamente');
+      res.status(201).send('Books creado exitosamente');
+    }
+  });
+});
+// Ruta para obtener todos los Libros
+app.get('/Books', (req, res) => {
+  const selectQuery = 'SELECT * FROM books';
+
+  client.execute(selectQuery, [], (err, result) => {
+    if (err) {
+      console.error('Error al obtener los Books:', err);
+      res.status(500).send('Error al obtener los Books');
+    } else {
+      // Renderiza la plantilla Pug y pasa los datos de usuarios a la vista
+      res.render('Books', { Authors: result.rows });
+    }
+  });
+});
+// Ruta para crear un nuevo Libro
+app.post('/Reviews', (req, res) => {
+  const { id, book, review, score, numberOfVotes} = req.body;
+
+  const insertQuery = `
+    INSERT INTO Reviews (id, book, review, score, numberOfVotes)
+    VALUES (?, ?, ?, ? ,?)
+  `;
+
+  client.execute(insertQuery, [id, book, review, score, numberOfVotes], { prepare: true }, (err) => {
+    if (err) {
+      console.error('Error al crear el Reviews:', err);
+      res.status(500).send('Error al crear el Reviews');
+    } else {
+      console.log('Reviews creado exitosamente');
+      res.status(201).send('Reviews creado exitosamente');
+    }
+  });
+});
+// Ruta para obtener todos los Libros
+app.get('/Reviews', (req, res) => {
+  const selectQuery = 'SELECT * FROM Reviews';
+
+  client.execute(selectQuery, [], (err, result) => {
+    if (err) {
+      console.error('Error al obtener los Reviews:', err);
+      res.status(500).send('Error al obtener los Reviews');
+    } else {
+      // Renderiza la plantilla Pug y pasa los datos de usuarios a la vista
+      res.render('Reviews', { Authors: result.rows });
+    }
+  });
+});
+
+// Ruta para crear un nuevo Review
+app.post('/sales', (req, res) => {
+  const { id, book, year, sales} = req.body;
+
+  const insertQuery = `
+    INSERT INTO salesbyyear (id, book,  year, sales)
+    VALUES (?, ?, ?, ?)
+  `;
+
+  client.execute(insertQuery, [id, book,  year, sales], { prepare: true }, (err) => {
+    if (err) {
+      console.error('Error al crear el Sales:', err);
+      res.status(500).send('Error al crear el Sales');
+    } else {
+      console.log('Sales creado exitosamente');
+      res.status(201).send('Sales creado exitosamente');
+    }
+  });
+});
+// Ruta para obtener todos los Review
+app.get('/sales', (req, res) => {
+  const selectQuery = 'SELECT * FROM salesbyyear';
+
+  client.execute(selectQuery, [], (err, result) => {
+    if (err) {
+      console.error('Error al obtener los Sales:', err);
+      res.status(500).send('Error al obtener los Sales');
+    } else {
+      // Renderiza la plantilla Pug y pasa los datos de usuarios a la vista
+      res.render('Sales', { Authors: result.rows });
+    }
+  });
+});
+
+// delete (not working)
 // Función para eliminar un usuario por su ID
 function eliminarUsuario(userId, callback) {
   const deleteQuery = `
